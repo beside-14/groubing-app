c926144f1af1600bb8608056c540d48a5029e95a
Seungmoo Kim <77320847+KimseungMoo@users.noreply.github.com>
2023-06-27 22:56:12 +0900

    프로젝트 재생성
    - react native cli

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module-resolver'],
-    {
-      root: ['.'],
-      extensions: ['ios.ts', 'android.ts', '.ts', '.json', '.tsx', '.ios.tsx', '.android.tsx', '.jsx', '.js'],
-    }
-  ]
 };
